a907b2131c0cf32e17a533c403a391c04acbac8a
ESPGranEdu <raplhwigum@gmail.com>
2020-04-13 02:36:28 +0200

    Properly configured

--- config_shortcut-kill-x/install.sh
+++ config_shortcut-kill-x/install.sh
@@ -9,20 +9,20 @@
 }
 
 if grep -qi "serverflags" /etc/X11/xorg.conf; then
-	sed -i '/ServerFlags/a \\tOption "DontZap" "off"' /etc/X11/xorg.conf
+	grep -Eq "\"DontZap\"\s\"[a-zA-Z]*\"" || sed -i '/ServerFlags/a \\tOption "DontZap" "off"' /etc/X11/xorg.conf
 else
 	echo -e "Section \"ServerFlags\"
-	\tOption \"DontZap\" \"off\"
-	EndSection" >>/etc/X11/xorg.conf
+\tOption \"DontZap\" \"off\"
+EndSection" >>/etc/X11/xorg.conf
 fi
 
 if [ -f /etc/X11/xorg.conf.d/00-keyboard.conf ]; then
-	! grep -q "terminate:ctrl_alt_bksp" /etc/X11/xorg.conf.d/00-keyboard.conf ||
+	grep -q "terminate:ctrl_alt_bksp" /etc/X11/xorg.conf.d/00-keyboard.conf ||
 		sed -i '/InputClass/a \\tOption "XkbOptions" "terminate:ctrl_alt_bksp"' /etc/X11/xorg.conf.d/00-keyboard.conf
 else
 	echo -e "Section \"InputClass\"
-	\tIdentifier \"system-keyboard\"
-	\tMatchIsKeyboard \"on\"
-	\tOption \"XkbOptions\" \"terminate:ctrl_alt_bksp\"
-	EndSection" >/etc/X11/xorg.conf.d/00-keyboard.conf
+\tIdentifier \"system-keyboard\"
+\tMatchIsKeyboard \"on\"
+\tOption \"XkbOptions\" \"terminate:ctrl_alt_bksp\"
+EndSection" >/etc/X11/xorg.conf.d/00-keyboard.conf
 fi
